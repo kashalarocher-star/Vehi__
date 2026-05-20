27db02fea3c3510dec0d82586d39bedba1fb3e90
kasha <kashalarocher@gmail.com>
2026-05-20 08:33:52 +0200

    creation et modification de mes fichiers

--- VehiTrack/forms.py
+++ VehiTrack/forms.py
@@ -1,20 +1,37 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Matricule
 
 class InscriptionForm(UserCreationForm):
+    matricule = forms.CharField(max_length=50, label="Matricule du vehicule")
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+
+    def clean_matricule(self):
+        code = self.cleaned_data['matricule']
+        try:
+            self.matricule_instance = Matricule.objects.get(code=code)
+        except Matricule.DoesNotExist:
+            raise forms.ValidationError("Ce matricule n'existe pas dans notre base.")
+        # Vérifier si le matricule a déjà un propriétaire
+        if self.matricule_instance.proprietaire is not None:
+            raise forms.ValidationError("Ce véhicule est déjà associé à un compte.")
+        return code
     def save(self, commit=True):
         user = super().save(commit=False)
+        user.username = self.cleaned_data['matricule']
         user.email = self.cleaned_data['email']
         # Nouvel utilisateur = simple (non staff)
         user.is_staff = False
         user.is_superuser = False
         if commit:
             user.save()
+            #associe l'utilisateur au matricule
+            self.matricule_instance.proprietaire = user
+            self.matricule_instance.save()
         return user
--- VehiTrack/models.py
+++ VehiTrack/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
 from django.contrib.auth.models import User
 from datetime import date, timedelta
 
 class Matricule(models.Model):
+    #ajouter les types de vehicule
+    TYPE_VEHICULE_CHOICES =[
+        ('camionnette', 'Camionnette'),
+        ('suv', 'Suv'),
+        ('camion', 'Camion'),
+        ('voiture', 'Voiture'),
+        ('autre', 'Autre'),
+    ]
+    type_vehicule = models.CharField(max_length=20, choices=TYPE_VEHICULE_CHOICES, null=True, blank=True)
     code = models.CharField(max_length=50, unique=True)
     nom = models.CharField(max_length=100)
-
+    proprietaire = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='vehicule', null=True, blank=True)
     def __str__(self):
         return f"{self.code} - {self.nom}"
 
